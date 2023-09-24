72131023048bf858f0d0f19ef81b33f040f56bd9
deukyeon <ghkdemrdus@gmail.com>
2023-09-24 19:32:29 +0900

    사용하지 않는 코드 제거 및 Project.swift 업데이트

--- Project.swift
+++ Project.swift
@@ -1,21 +1,26 @@
 import ProjectDescription
 
 let project = Project(
-    name: "LoveBird",
-    organizationName: "Prography",
-    targets: [
-        Target(
-            name: "LoveBird",
-            platform: .iOS,
-            product: .app,
-            bundleId: "com.lovebird.ios",
-            deploymentTarget: .iOS(targetVersion: "16.0", devices: .iphone),
-            infoPlist: "LoveBird/Info.plist",
-            sources: ["LoveBird/Sources/**", "LoveBird/Config.swift"],
-            resources: ["LoveBird/Resources/**"],
-            dependencies: [
-                .external(name: "ComposableArchitecture")
-            ]
-        )
-    ]
+  name: "LoveBird",
+  packages: [
+    .remote(
+      url: "https://github.com/pointfreeco/swift-composable-architecture",
+      requirement: .upToNextMajor(from: "0.59.0")
+    )
+  ],
+  targets: [
+    Target(
+      name: "LoveBird",
+      platform: .iOS,
+      product: .app,
+      bundleId: "com.lovebird.ios",
+      deploymentTarget: .iOS(targetVersion: "16.0", devices: .iphone),
+      infoPlist: "LoveBird/Info.plist",
+      sources: ["LoveBird/Sources/**", "LoveBird/Config.swift"],
+      resources: ["LoveBird/Resources/**"],
+      dependencies: [
+        .package(product: "ComposableArchitecture")
+      ]
+    )
+  ]
 )
